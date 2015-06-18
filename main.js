b0ee19b6005c792b3ef6f086855c7a4253bd7a55
Kyle Kelley <rgbkrk@gmail.com>
2015-06-17 21:00:11 -0500

    Launch the initial window based on the arg.

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 var app = require('app');  // Electron app
 var BrowserWindow = require('browser-window');  // Creating Browser Windows
 
-var jupyter = require("./lib/jupyter.js")
+var jupyter = require("./lib/jupyter.js");
 
 // Parse out a kernel-####.json argument
 var argv = require('minimist')(process.argv.slice(2));
-var connFile = argv._[0];
-var config = require(connFile);
+
+var connFileArg;
+if (argv._.length > 0) {
+  connFileArg = argv._[0];
+}
 
 // Report crashes to our server.
 require('crash-reporter').start();
-
-// Keep a global reference of the side car window object
-// If we don't, the window will be closed automatically when the javascript
-// object is GCed.
-var sideCar = null;
-var session = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -24,9 +21,14 @@ app.on('window-all-closed', function() {
 });
 
 
-// This method will be called when Electron has done every
-// initialization and is ready for creating browser windows.
-app.on('ready', function() {
+function launchSideCar(connFile) {
+  // Keep a global reference of the side car window object
+  // If we don't, the window will be closed automatically when the javascript
+  // object is GCed.
+  var config = require(connFile);
+  var sideCar = null;
+  var session = null;
+
   // Create the browser window.
   sideCar = new BrowserWindow({
     width: 800,
@@ -37,9 +39,9 @@ app.on('ready', function() {
 
   // and load the index.html of the app.
   sideCar.loadUrl('file://' + __dirname + '/index.html');
-  
+
   sideCar.webContents.on('did-finish-load', function() {
-    
+
     session = new jupyter.IOPubSession(config, function(msg){
       // Get display data if available
       if("content" in msg && "data" in msg.content) {
@@ -49,7 +51,7 @@ app.on('ready', function() {
 
     });
   });
-  
+
   // Emitted when the window is closed.
   sideCar.on('closed', function() {
     // Dereference the window object, usually you would store windows
@@ -57,4 +59,14 @@ app.on('ready', function() {
     // when you should delete the corresponding element.
     sideCar = null;
   });
+}
+
+// This method will be called when Electron has done every
+// initialization and is ready for creating browser windows.
+app.on('ready', function() {
+  if (connFileArg) {
+    launchSideCar(connFileArg);
+  } else {
+    console.log("Didn't specify a running kernel.");
+  }
 });
