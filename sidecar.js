81da0e87976e3900436fe1bf795e183d3dabc88f
Ash Wilson <smashwilson@gmail.com>
2015-06-26 21:18:35 -0500

    Move behavior into the template files, too.

--- sidecar.js
+++ sidecar.js
@@ -1,39 +1,5 @@
 // This code executes within the sidecar window.
 
-var resultPrototype = Object.create(HTMLElement.prototype, {
-  createdCallback: {
-    value: function() {
-      var t = document.querySelector('#tmpl-result');
-      var clone = document.importNode(t.content, true);
-      this.root = this.createShadowRoot();
-      this.root.appendChild(clone);
-    }
-  }
-});
-
-resultPrototype.setPayload = function (content) {
-  this.root.getElementById("output-container").innerHTML = content;
-};
-
-document.registerElement('sidecar-result', {prototype: resultPrototype});
-
-var errPrototype = Object.create(HTMLElement.prototype, {
-  createdCallback: {
-    value: function() {
-      var t = document.querySelector('#tmpl-err');
-      var clone = document.importNode(t.content, true);
-      this.root = this.createShadowRoot();
-      this.root.appendChild(clone);
-    }
-  }
-});
-
-errPrototype.setPayload = function (trace) {
-  this.root.getElementById("trace").innerHTML = trace;
-};
-
-document.registerElement('sidecar-err', {prototype: errPrototype});
-
 var ipc = require('ipc');
 var container = document.getElementById('container');
 
